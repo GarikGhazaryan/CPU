d420863f2f39672ab813391fbaa3a5a481383b3d
Garik <garikghazaryan1983@gmail.com>
2023-05-12 10:00:52 +0400

    read write

--- CPU.cpp
+++ CPU.cpp
@@ -3,8 +3,8 @@
 
 class RAM{
 	int data_wr;
-	int data_r2;
-	int data_r3;
+	int data_r2=8;
+	int data_r3=9;
 	int instr1=9999;
 	int instr2=7777;
 	int instr3=444;
@@ -12,17 +12,25 @@ class RAM{
 
 		int read(int adres){
 			switch(adres){
-				case 1:
+				case 55:
 					return data_wr;
-				case 2:
+				case 0:
 					return data_r2;
-				case 3:
+				case 15:
 					return data_r3;
-				case 4:
-					return instr1;
-				case 5:
-					return instr2;
 				case 6:
+					return data_r2;
+				case 1:
+					return data_r3;
+				case 11:
+					return data_r2;
+				case 12:
+					return data_r3;
+				case 100:
+					return instr1;
+				case 101:
+					return instr2;
+				case 102:
 					return instr3;
 			}
 			return 0;
@@ -30,12 +38,15 @@ class RAM{
 
 		void write(int adres,int data){
 			switch(adres){
+				case 7:
+					data_wr=data;
+					break;
+				case 14:
+					data_wr=data;
+					break;
 				case 1:
 					data_wr=data;
-				case 2:
-					data_r2=data;
-				case 3:
-					data_r3=data;
+					break;
 		}
 	}
 };
@@ -61,9 +72,9 @@ class CU{
 	int r_wr;
 	int r2;
 	int r3;
-	int cir=4;     //current instruction adres register
-	int rdat1=8;
-	int rdat2=9;
+	int cir=100;     //current instruction adres register
+	int rdat1;
+	int rdat2;
 	int rdat_wr;
 	public:
 		void fetch(){
@@ -82,9 +93,15 @@ class CU{
 
 		void execute(){
 			ALU alu;
+			RAM ram;
+			rdat1=ram.read(r2);
+			rdat2=ram.read(r3);
 			rdat_wr=alu.perform(op,rdat1,rdat2);
+			ram.write(r_wr,rdat_wr);
 
-			std::cout<<rdat_wr<<std::endl;
+
+			std::cout<<"data_wr - "<<ram.read(55)<<std::endl;
+			//std::cout<<"rdat_wr - "<<rdat_wr<<std::endl;
 		}
 
 
@@ -96,11 +113,11 @@ class CU{
 			std::cout << "r2 - "<< r2<< std::endl;
 			std::cout << "r3 - "<< r3<< std::endl;
 			
-			std::cout << std::bitset<sizeof(inst) * 4>(inst) << std::endl;
-			std::cout << std::bitset<sizeof(op) * 4>(op) << std::endl;
-			std::cout << std::bitset<sizeof(r_wr) * 4>(r_wr) << std::endl;
-			std::cout << std::bitset<sizeof(r2) * 4>(r2) << std::endl;
-			std::cout << std::bitset<sizeof(r3) * 4>(r3) << std::endl;
+	//		std::cout << std::bitset<sizeof(inst) * 4>(inst) << std::endl;
+	//		std::cout << std::bitset<sizeof(op) * 4>(op) << std::endl;
+	//		std::cout << std::bitset<sizeof(r_wr) * 4>(r_wr) << std::endl;
+	//		std::cout << std::bitset<sizeof(r2) * 4>(r2) << std::endl;
+	//		std::cout << std::bitset<sizeof(r3) * 4>(r3) << std::endl;
 		}
 };
 
@@ -112,7 +129,7 @@ class CPU{
 			cu.fetch();
 			cu.decod();
 			cu.execute();
-			//cu.print();
+			cu.print();
 		}
 };
 
