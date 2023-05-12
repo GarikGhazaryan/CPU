134614df148aa6393bd4d888128e21d42902e297
Garik <garikghazaryan1983@gmail.com>
2023-05-12 08:22:01 +0400

    by cicle

--- CPU.cpp
+++ CPU.cpp
@@ -10,69 +10,66 @@ class RAM{
 	int instr3=444;
 	public:
 
-	int read(int adres){
-		switch(adres){
-			case 1:
-					return data_wr;
-			case 2:
-					return data_r2;
-			case 3:
-					return data_r3;
-			case 4:
-					return instr1;
-			case 5:
-					return instr2;
-			case 6:
-					return instr3;
-		}
-		return 0;
-	}
-
-	void write(int adres,int data){
-		switch(adres){
+		int read(int adres){
+			switch(adres){
 				case 1:
-						data_wr=data;
+					return data_wr;
 				case 2:
-						data_r2=data;
+					return data_r2;
 				case 3:
-						data_r3=data;
+					return data_r3;
+				case 4:
+					return instr1;
+				case 5:
+					return instr2;
+				case 6:
+					return instr3;
+			}
+			return 0;
+		}
+
+		void write(int adres,int data){
+			switch(adres){
+				case 1:
+					data_wr=data;
+				case 2:
+					data_r2=data;
+				case 3:
+					data_r3=data;
 		}
 	}
 };
 
-	class ALU{
-
-		public:
-			int perform(int op, int dat1, int dat2){ 
-				switch(op){
-	        	    case 0:
-							return dat1*dat2;
-	        	    case 1:
-	        	            return dat1-dat2;
-	        	    case 2:
-	        	            return dat1+dat2;
-	        	}
-	        	return 0;
+class ALU{
+	public:
+		int perform(int op, int dat1, int dat2){ 
+			switch(op){
+				case 0:
+					return dat1*dat2;
+				case 1:
+					return dat1-dat2;
+				case 2:
+					return dat1+dat2;
 			}
-	};
+			return 0;
+		}
+};
 	
-	class CU{
-		public:	
-			int inst;
-			int op;
-			int r_wr;
-			int r2;
-			int r3;
-			int cir=4;     //current instruction adres register
-			int rdat1=8;
-			int rdat2=9;
-			int rdat_wr;
-
-
-	
+class CU{
+	int inst;
+	int op;
+	int r_wr;
+	int r2;
+	int r3;
+	int cir=4;     //current instruction adres register
+	int rdat1=8;
+	int rdat2=9;
+	int rdat_wr;
+	public:
 		void fetch(){
 			RAM ram;
 			inst=ram.read(cir);	
+			cir++;
 		}
 	
 		void decod(){
@@ -85,7 +82,9 @@ class RAM{
 
 		void execute(){
 			ALU alu;
-			std::cout<<alu.perform(op,rdat1,rdat2)<<std::endl;
+			rdat_wr=alu.perform(op,rdat1,rdat2);
+
+			std::cout<<rdat_wr<<std::endl;
 		}
 
 
@@ -103,7 +102,7 @@ class RAM{
 			std::cout << std::bitset<sizeof(r2) * 4>(r2) << std::endl;
 			std::cout << std::bitset<sizeof(r3) * 4>(r3) << std::endl;
 		}
-	};
+};
 
 
 class CPU{
@@ -122,5 +121,8 @@ class CPU{
 int main(){
 	
 		CPU cpu;
+		CU cu;
+		for(int i=0;i<3;i++){
 		cpu.load();
+		}
 }
